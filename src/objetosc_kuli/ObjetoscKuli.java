a9054f334b48dede01dfa5c48e43369f2acd44bf
kwarchal <kamilwarchal295@gmail.com>
2023-12-01 09:31:13 +0100

    objetosc kuli

---
+++ src/objetosc_kuli/ObjetoscKuli.java
@@ -0,0 +1,26 @@
+package objetosc_kuli;
+
+import java.io.*;
+
+public class ObjetoscKuli {
+
+    public static void main(String[] args)
+            throws IOException
+    {
+        double r, objetosc;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("program oblicza objetosc kuli.");
+        System.out.println("podaj promien kuli:");
+
+        r = Double.parseDouble(br.readLine());
+
+        objetosc = 4 * Math.PI * r * r * r/3;
+
+        System.out.print("objetosc kuli o promieniu r = ");
+        System.out.printf("%2.2f.", r );
+        System.out.print("wynosi: ");
+        System.out.printf("%2.2f.", objetosc);
+    }
+}
